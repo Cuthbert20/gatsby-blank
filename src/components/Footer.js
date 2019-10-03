e79b8759da83e0f8fabdc906d7d64ee61803c2cc
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-03 13:27:37 -0600

    added headshot from cloudinary, created footer and added content and styling

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import React from "react"
+import "./Footer.css"
+import { IoIosMailOpen } from "react-icons/io"
+import { FaLaptopCode } from "react-icons/fa"
+
+export default () => (
+  <footer className="footer">
+    <div className="footer-container">
+      <div className="techs">
+        <FaLaptopCode size={40} />
+        <h3 className="footer-title">Technology</h3>
+      </div>
+      <div className="list">
+        <ul>
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SASS</li>
+          <li>Styled Components</li>
+          <li>PostgreSQL</li>
+          <li>Express</li>
+          <li>Jest</li>
+          <li>BootStrap 4</li>
+          <li>Gatsby</li>
+        </ul>
+      </div>
+    </div>
+    <div className="footer-container">
+      <div className="contact">
+        <IoIosMailOpen size={40} />
+        <h3 className="footer-title">Contact Info</h3>
+      </div>
+      <div className="contact-list">
+        <img
+          className="profile-img"
+          src="https://res.cloudinary.com/cuthbert20/image/upload/v1570130386/spencersmallphoto_ap5ay8.png"
+          alt=""
+        />
+        <ul>
+          <li>
+            Email:{" "}
+            <a href="mailto:knowltonspencer@gmail.com">
+              knowltonspencer@gmail.com
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,10 @@
 import React from "react"
 import Header from "../components/Header"
+import Footer from "../components/Footer"
 
-export default () => <Header />
+export default () => (
+  <div>
+    <Header />
+    <Footer />
+  </div>
+)

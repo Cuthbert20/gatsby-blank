84ffbd89c6b6d90c5e02505807d20d79a28736f4
Cuthbert20 <knowltonspencer@gmail.com>
2019-10-04 16:53:12 -0600

    added main component

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Main.css"
+
+export default () => (
+  <div>
+    <h1>Main Component</h1>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Main from "../components/Main"
 
 export default () => (
-  <div>
+  <div className="body-style">
     <Header />
+
     <Footer />
   </div>
 )
